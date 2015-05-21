378e27029fed3db74d97ff950366c60e336ad1e8
cassdotcom <cassdotcom@gmail.com>
2015-05-22 00:58:17 +0100

    more gui

--- scripts/pshellgui.ps1
+++ scripts/pshellgui.ps1
@@ -1,13 +1,13 @@
 #Required to load the XAML form and create the PowerShell Variables
 
-.\loadDialog.ps1 -XamlPath 'C:\Users\cass\Documents\Visual Studio 2013\Projects\pshell_gui\forms\CBMForm.xaml'
-
+#.\loadDialog.ps1 -XamlPath 'C:\Users\cass\Documents\Visual Studio 2013\Projects\pshell_gui\forms\CBMForm.xaml'
+.\loadDialog.ps1 -XamlPath 'C:\Users\cass\Documents\Visual Studio 2013\Projects\pshell_gui\forms\titleForm.xml'
 
 #EVENT Handler
 
-$button1.add_Click({
+$bt_CBM.add_Click({
 
- $Label1.Content = "Hello World"
+ $logo_TITLE.Content = "Hello World"
 
 })
 
