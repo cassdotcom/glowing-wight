3d7c37af4c704487e4d2aaea6da0d33ffa21258b
cassdotcom <cassdotcom@users.noreply.github.com>
2015-05-28 02:09:46 +0100

    Update pshellgui.ps1

--- scripts/pshellgui.ps1
+++ scripts/pshellgui.ps1
@@ -31,18 +31,19 @@
 
 Process { 
 
-	$dbase = "C:\Users\ac00418\Documents\CBM_repo\model_data\NetworkModels.mdb"
-	$qry = "Select NAME from NetworkModels"
-	
-	start-sleep 3
+	# THE S:\ DRIVE WILL HAVE A REPOSITORY FOR THESE SETTINGS!!!!!!!!
+	$BIG_DATA = New-Object PSObject 
+	$BIG_DATA | Add-Member -MemberType NoteProperty -Name GUI_SETTINGS_PATH -Value "C:\Users\ac00418\Documents\gui\sandbox\settings"
+	$BIG_DATA | Add-Member -MemberType NoteProperty -Name CBM_SETTINGS_PATH -Value "C:\Users\ac00418\Documents\CBM_repo\settings"
+	$BIG_DATA | Add-Member -MemberType NoteProperty -Name XAML_PATH -Value "C:\Users\ac00418\Documents\gui\sandbox"
 	
 	# Load aliases
-	$alias_file = gc "C:\Users\ac00418\Documents\gui\sandbox\settings\set-cbm-alias.txt"
-	
-	foreach ( $al in $alias_file ) { set-alias ($al.split(","))[0] ($al.split(","))[1]}
+	$alias_file = gc "$($BIG_DATA.GUI_SETTINGS_PATH)\set-cbm-alias.txt"
+	start-sleep 5 
+	foreach ( $al in $alias_file ) { set-alias ($al.split(","))[0] ($al.split(","))[1] }
 	
 	#Required to load the XAML form and create the PowerShell Variables
-	.\loadDialog.ps1 -XamlPath 'MainForm.xaml'
+	load_XAML -XamlPath "$($BIG_DATA.XAML_PATH)\MainForm.xaml"
 	
 	# Print title
 	$timedate = Get-Date -UFormat "%A %e %B %Y, %R"
@@ -50,10 +51,10 @@ Process {
 	$tx_dets.AppendText("`r`n$($timedate)")
 
 	#EVENT Handler
-	$bt_CBM.add_Click({ $rOK = .\create-cbm\button-cbm.ps1 })
-	$bt_VERIFY.add_Click({ $rOK = .\create-cbm\button-verify.ps1 $tx_dets })
-	$bt_VIEW.add_Click({ $rOK = .\create-cbm\button-view.ps1 $tx_dets })
-	$bt_EXIT.add_Click({ button_exit $tx_dets })
+	$bt_CBM.add_Click({ $rOK = button-cbm })
+	$bt_VERIFY.add_Click({ $rOK = button-verify })
+	$bt_VIEW.add_Click({ $rOK = button-view })
+	$bt_EXIT.add_Click({ exit })
 
 	#Launch the window
 	$xamGUI.ShowDialog() | out-null
@@ -62,65 +63,11 @@ Process {
 
 
 Begin {
-
-function qry-database {
-
-	Param (
-		# Database name
-		[Parameter(Position = 0, Mandatory = $true)]
-		[System.String]
-		$dbase,
-		# Query to database
-		[Parameter(Position = 1, Mandatory = $true)]
-		[System.String]
-		$qry
-	)
-		
-	# this holds the results
-	$dbase_return = @()
-
-	# Generic variables
-	$OpenStatic = 3
-	$LockOptimistic = 3
 	
-	# create connection to database
-	$conn = New-Object -ComObject ADODB.Connection
-	# create recordset to hold return values
-	$rs = New-Object -ComObject ADODB.Recordset
+	function button-exit {
 	
-	# open connection
-	$conn.Open("Provider = Microsoft.Jet.OLEDB.4.0;Data Source=$dbase")
-	
-	# query
-	$rs.Open($qry, $conn, $OpenStatic, $LockOptimistic)
-	
-	# read from recordset
-	while ( !$rs.EOF ) {
-		$model_info = New-Object PSObject
-		foreach ($field in $rs.Fields) {
-			$model_info | Add-Member -MemberType NoteProperty -Name $($field.Name) -Value $field.Value
-		}
-		$rs.MoveNext()
-		$dbase_return += $model_info
-	}
-
-	# Close the connection 
-	$rs.Close()
-	$conn.Close()
-	
-	return $dbase_return
-}
-	
-	
-	function button_exit {
-	
-		Param (
-			[System.Windows.Controls.RichTextBox]
-			$tx_dets
-		)
-		
 		$box_check = New-Object System.Windows.Forms.Form
-		$box_check.width = 500
+		$box_check.width = 300
 		$box_check.height = 150
 		$box_check.startposition = [System.Windows.Forms.FormStartPosition]::CenterScreen
 		
@@ -131,12 +78,22 @@ function qry-database {
 		
 		$bt_no = New-Object System.Windows.Forms.Button
 		$bt_no.Left = 30
-		$bt_no.Top = 90
+		$bt_no.Top = 50
 		$bt_no.Width = 100
 		$bt_no.Text = "NO"
 		
+		$bt_yes = New-Object System.Windows.Forms.Button
+		$bt_yes.Left = 160
+		$bt_yes.Top = 50
+		$bt_yes.Width = 100
+		$bt_yes.Text = "YES"
+		
 		$box_check.Controls.Add($box_label)
 		$box_check.Controls.Add($bt_no)
+		$box_check.Controls.Add($bt_yes)
+		
+		$bt_no.Add_Click({ break })
+		$bt_yes.Add_Click({  })
 		
 		$box_check.ShowDialog()		
 	}
